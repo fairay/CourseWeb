571d3d0fc1a4ccbf02e455e6f648113aa25aa376
fairay <48217178+fairay@users.noreply.github.com>
2021-10-18 00:37:56 +0300

    added create sql query

---
+++ sql/create.sql
@@ -0,0 +1,58 @@
+DROP TABLE IF EXISTS Accounts CASCADE;
+CREATE TABLE IF NOT EXISTS Accounts(
+	login 			VARCHAR(64)		PRIMARY KEY,
+	role 			VARCHAR(15) 	NOT NULL,
+	salt 			VARCHAR(64)		NOT NULL,
+	hashed_password	VARCHAR(64)		NOT NULL
+);
+
+DROP TABLE IF EXISTS Ingredients CASCADE;
+CREATE TABLE IF NOT EXISTS Ingredients(
+	title 			VARCHAR(64)		PRIMARY KEY
+);
+
+DROP TABLE IF EXISTS Categories CASCADE;
+CREATE TABLE IF NOT EXISTS Categories (
+	title 			VARCHAR(64)		PRIMARY KEY
+);
+
+DROP TABLE IF EXISTS Recipes CASCADE;
+CREATE TABLE IF NOT EXISTS Recipes (
+	id				SERIAL 			PRIMARY KEY,
+	author			VARCHAR(64)		REFERENCES Accounts(login) NOT NULL,
+	title 			VARCHAR(128)	NOT NULL,
+	created_at		TIMESTAMP		NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	description		TEXT			NOT NULL,
+	duration		TIME,
+	portion_num		INT
+);
+
+DROP TABLE IF EXISTS Steps CASCADE;
+CREATE TABLE IF NOT EXISTS Steps (
+	recipe			INT 			REFERENCES Recipes(id) NOT NULL,
+	num				SERIAL			NOT NULL,
+	description		TEXT			NOT NULL,
+	title 			VARCHAR(64),
+	PRIMARY KEY (recipe, num)
+);
+
+DROP TABLE IF EXISTS Grades CASCADE;
+CREATE TABLE IF NOT EXISTS Grades (
+	recipe			INT 			REFERENCES Recipes(id) NOT NULL,
+	account			VARCHAR(64)		REFERENCES Accounts(login) NOT NULL,
+	PRIMARY KEY (recipe, account)
+);
+
+DROP TABLE IF EXISTS RecipeСategory CASCADE;
+CREATE TABLE IF NOT EXISTS RecipeCategory (
+	recipe			INT 			REFERENCES Recipes(id) NOT NULL,
+	category		VARCHAR(64)		REFERENCES Categories(title) NOT NULL,
+	PRIMARY KEY (recipe, category)
+);
+
+DROP TABLE IF EXISTS RecipeIngredient CASCADE;
+CREATE TABLE IF NOT EXISTS RecipeIngredient (
+	recipe			INT 			REFERENCES Recipes(id) NOT NULL,
+	item			VARCHAR(64)		REFERENCES Ingredients(title) NOT NULL,
+	PRIMARY KEY (recipe, item)
+);
