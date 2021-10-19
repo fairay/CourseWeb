0f1556b85acfae88a8654a1219644fe665ea60ff
Bryanskaya <bryanskayakatya@yandex.ru>
2021-10-20 01:06:44 +0300

    ADDED: test for connection with DB

--- server/go.mod
+++ server/go.mod
@@ -2,4 +2,22 @@ module app/server
 
 go 1.17
 
-require github.com/gorilla/mux v1.8.0 // indirect
+require (
+	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
+	github.com/jackc/pgconn v1.10.0 // indirect
+	github.com/jackc/pgio v1.0.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgproto3/v2 v2.1.1 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20200714003250-2b9c44734f2b // indirect
+	github.com/jackc/pgtype v1.8.1 // indirect
+	github.com/jackc/pgx/v4 v4.13.0 // indirect
+	github.com/jinzhu/gorm v1.9.16 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.2 // indirect
+	github.com/lib/pq v1.10.3 // indirect
+	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
+	golang.org/x/text v0.3.7 // indirect
+	gorm.io/driver/postgres v1.1.2 // indirect
+	gorm.io/gorm v1.21.16 // indirect
+)
--- server/main.go
+++ server/main.go
@@ -1,26 +1,59 @@
 package main
 
 import (
+	"app/server/repository/objects"
 	"encoding/json"
 	"fmt"
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
 
-func main()  {
+var db *gorm.DB
+
+/*var e error*/
+
+func main() {
+	dsn := "host=localhost user=admin password=admin dbname=recipes port=5432 sslmode=disable"
+	db, e := gorm.Open("postgres", dsn)
+
+	if e != nil {
+		fmt.Println(e)
+	} else {
+		fmt.Println("Connection Established")
+	}
+
+	defer db.Close()
+	db.SingularTable(true)
+	db.AutoMigrate(&objects.Categories{})
+
 	fmt.Println("Hello test")
+	GetAllCategories()
 
 	router := mux.NewRouter()
 	router.HandleFunc("/test", getTest).Methods("GET")
+	//router.HandleFunc("/categories", GetAllCategories).Methods("GET")
 	http.ListenAndServe(":1991", router)
 }
 
 func getTest(w http.ResponseWriter, r *http.Request) {
-	var data = [...]int{1, 2, 3, 4}
+	var data = [...]int{155555, 2, 3, 4}
 
 	w.Header().Set("Content-Type", "application/json")
 	w.Header().Set("Response-Code", "00")
 	w.Header().Set("Response-Desc", "Success")
 	json.NewEncoder(w).Encode(data)
+}
+
+func GetAllCategories() {
+	temp := []objects.Categories{}
+	db.Find(&temp)
+	fmt.Println(temp)
+
+	/*w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Response-Code", "00")
+	w.Header().Set("Response-Desc", "Success")
+	json.NewEncoder(w).Encode(temp)*/
 }
