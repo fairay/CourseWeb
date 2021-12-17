46cc25fc064c3549ae6efebcd045c6885e3552f5
Bryanskaya <bryanskayakatya@yandex.ru>
2021-12-17 19:58:25 +0300

    STARTED: ingredients

--- front/src/components/layout/home/menuright/recipe/Recipe.tsx
+++ front/src/components/layout/home/menuright/recipe/Recipe.tsx
@@ -15,48 +15,49 @@ interface RecipeProps {}
 
 const Recipe: React.FC<RecipeProps> = (props) => {
     return (
-        <HStack
-        w="100%"
-        borderWidth="1px"
-        borderStyle="solid"
-        borderRadius="10px"
-        borderColor="title"
-        alignItems="stretch"
+        <Link style={{ textDecoration: 'none' }}
+            href="#"
         >
-            <Image src={photoRecipe} style={{width: 200,
-                                            height: 300,
-                                            borderTopLeftRadius: 10,
-                                            borderBottomLeftRadius: 10,
-            }} 
-            />
+            <HStack
+                w="480px"
+                borderWidth="1px"
+                borderStyle="solid"
+                borderRadius="10px"
+                borderColor="title"
+                alignItems="stretch"
+            >
+                <Image src={photoRecipe} style={{width: 200,
+                                                height: 300,
+                                                borderTopLeftRadius: 10,
+                                                borderBottomLeftRadius: 10,
+                }} 
+                />
 
-            <Box display="flex" justifyContent="space-between" flexDirection="column">
-                <VStack>
-                    <Box w="100%" h="77">
-                        <Text textStyle="subtitle" color="title" align="center">
-                        Торт Графские развалины
-                        </Text>
-                    </Box>
+                <Box 
+                    display="flex" 
+                    justifyContent="space-between" 
+                    flexDirection="column"
+                >
+                    <VStack>
+                        <Box w="100%" h="77">
+                            <Text textStyle="subtitle" color="title" align="center">
+                            Торт Графские развалины
+                            </Text>
+                        </Box>
 
-                    <Box>
-                        <Text  textStyle="alt-body" color="text">
-                        Необычайно вкусный торт, не пожалеете Приготовить безе. В миску вылить белки и добавить 240 грамм сахара. Установить миску на водяную баню и, энергично помешивая массу венчиком, дать сахару бла-бла
-                        </Text>
-                    </Box>
-                </VStack>
+                        <Box>
+                            <Text  textStyle="alt-body" color="text">
+                            Необычайно вкусный торт, не пожалеете Приготовить безе. В миску вылить белки и добавить 240 грамм сахара. Установить миску на водяную баню и, энергично помешивая массу венчиком, дать сахару бла-бла
+                            </Text>
+                        </Box>
+                    </VStack>
 
-                <Text>
-                    Важная инфа в два овала
-                </Text>
-            </Box>
-
-            
-
-            
-
-
-
-        </HStack>
+                    <Text>
+                        Важная инфа в два овала
+                    </Text>
+                </Box>
+            </HStack>
+        </Link>
     )
 }
 
