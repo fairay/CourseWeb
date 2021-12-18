71758ff3780b5934efec02254c89af10becf07ad
Bryanskaya <bryanskayakatya@yandex.ru>
2021-12-18 19:18:22 +0300

    MOVED: recipe

--- front/src/components/layout/home/allrecipes/recipe/RecipeDetailed.tsx
+++ front/src/components/layout/home/allrecipes/recipe/RecipeDetailed.tsx
@@ -26,48 +26,7 @@ const RecipeDtl: React.FC<RecipeDtlProps> = (props) => {
     const [hide, show] = React.useState(true);
 
     return (
-        <Box display="flex" w="100%">
-            <HStack position="relative" paddingRight="15px">
-                <Image src={photoRecipe} 
-                    borderRadius="10px" 
-                    width="336" height="400"
-                />
-
-                <VStack>
-                    <Box position="absolute" right="0px" top="0px"> <DeleteIcon width="50px" height="40px" /> </Box>
-                    
-                    <Button display="contents" onClick={() => show(!hide)}>
-                        <Box position="absolute" right="0px" bottom="0px">
-                            {hide && <EmptyLike width="50px" height="40px" />}
-                            {!hide && <FullLike width="50px" height="40px" />}
-                        </Box>
-                    </Button>
-                </VStack>
-            </HStack>
-
-            <Box>
-                <HStack display="flex" justify-content="space-between">
-                    <RoundBox width="120px" height="30px"
-                        borderColor="accent-1" alignItems="center"> 
-                            <Box marginY="auto" marginX="8px"> <ClockIcon /> </Box>
-                            <Text marginLeft="2px" textStyle="body"> TODO </Text>
-                    </RoundBox>
-
-                    <RoundBox width="120px" height="30px"
-                        borderColor="accent-1" alignItems="center"> 
-                            <Box marginY="auto" marginX="8px"> <FullLike /> </Box>
-                            <Text marginLeft="2px" textStyle="body">1 024</Text>
-                    </RoundBox>
-
-                    <RoundBox width="120px" height="30px"
-                        borderColor="accent-1" alignItems="center"> 
-                            <Box marginY="auto" marginX="8px"> <PlateIcon /> </Box>
-                            <Text marginLeft="2px" textStyle="body">TODO</Text>
-                    </RoundBox>
-                </HStack>
-            </Box>
-            
-        </Box>
+        <Box display="flex" w="100%"></Box>
     )
 }
 
